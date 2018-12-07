ea6f032842e301495a5f1c5dd52288bfbdf1b9e4
lguild <massrubydev@gmail.com>
2018-12-07 11:10:33 -0500

    make the id checker more robust even if sample data does not require it to be

--- id_checker.gemspec
+++ id_checker.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'id_checker'
-  s.version = "0.1.3"
+  s.version = "0.1.4"
   s.date = '2018-12-07'
   s.summary = 'check for duplicate ids in CSV'
   s.files = [
--- lib/id_checker.rb
+++ lib/id_checker.rb
@@ -1,51 +1,109 @@
+
+
 require 'csv'
 require 'phonetic'
 
 module IdChecker
 
+  class Row
+
+    def to_hash
+      @row
+    end
+
+    def phone
+      @row['phone']
+    end
+
+    def email
+      @row['email']
+    end
+
+    def addr
+      @row['address'].to_s + @row['city'].to_s + @row['zip'].to_s
+    end 
+
+    def initialize(csv_row)
+      @row = {} 
+      csv_row.headers.each{|fld| @row[fld] = csv_row[fld]}
+    end
+
+    def [](ind)
+      @row[ind]
+    end
+  end
+ 
   class ResultSet
+    def initialize(attr)
+      @out = attr[:out]
+      @data = attr[:data]
+     end
 
-  	def initialize(attr)
-  	  @out = attr[:out]
-  	  @data = attr[:data]
-    end
-
-    def out
+     def out
       @out
-    end
+     end
 
-    def data
+     def data
       @data
-    end
+     end
 
   end
 
   class FileReader
-    def self.read(fpath)
-      out = ''
-      map = {}
-      data = {uniq: [], dup: []}
 
-	  CSV.foreach(fpath, :headers => true) do |row|
-		ky = row['first_name'].metaphone + row['last_name'].metaphone
-		row_el = {} 
-		row.headers.each{|fld| row_el[fld] = row[fld]}
-		map[ky] = map[ky] ? map[ky].push(row_el) : [row_el]  
-	  end
 
-	  map.keys.each do |element|
-	    if map[element].length > 1
-	  	  data[:dup] << map[element]
-	      out << 'dupes:' + map[element].inspect + "\n\n"
-	    else
-	      data[:uniq] << map[element][0]    
-	    end
-	  end
-
-      out << "UNIQUE:\n\n" + data[:uniq].join("\n\n")
-      out << "\n\n=============================\n\n"
-      out << "DUPLICATES:\n\n" + data[:dup].join("\n\n")
-      ResultSet.new(data: data, out: out)
+   def self.match_el(el, rows) 
+    matched = false
+    rows.each do |row|
+      if el.phone == row.phone
+        matched = true
+      elsif el.addr == row.addr
+        matched = true
+      elsif el.phone == row.email
+        matched = true
+      end
     end
+    matched = true if !matched && el.phone.blank? && el.addr.blank? && el.email.blank? 
+    matched
+  end
+     
+
+  def self.read(fpath)
+    out = ''
+    map = {}
+    data = {uniq: [], dup: []}
+
+    CSV.foreach(fpath, :headers => true) do |row|
+      ky = row['first_name'].metaphone + row['last_name'].metaphone
+      row_el = Row.new(row)
+      if map[ky] && match_el(row_el, map[ky])
+        map[ky].push(row_el)
+      else
+        map[ky] = [row_el]
+      end
+    end
+
+     map.keys.each do |element|
+      if map[element].length > 1
+        data[:dup] << map[element].map(&:to_hash)
+      else
+        data[:uniq] << map[element][0].to_hash    
+      end
+      end
+
+    out << "UNIQUE:\n\n" 
+    data[:uniq].each{|r| out << r.to_yaml + "\n\n"}
+    out << "\n\n=============================\n\n"
+    out << "DUPLICATES:\n\n"
+    data[:dup].each_with_index do |ar,idx|
+      out << "Duplicate set #{idx.to_s}:\n\n"
+      ar.each{|r| out << r.to_yaml + "\n\n"}
+    end 
+    ar = data[:uniq]
+    check = ar.map{|el| el['id']}.detect{|e| ar.rindex(e) != ar.index(e)}
+    out << "\n\nFAILED FOR ID #{check} ! ! !" if check
+    ResultSet.new(data: data, out: out)
+   end
   end
 end
+
